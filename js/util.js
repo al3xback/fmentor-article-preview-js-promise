1c45a5cbf6a1bbf9b55c208805ad66f2a4c3db56
alex <al3xback@gmail.com>
2024-02-03 19:51:08 +0700

    filled alt image

--- js/util.js
+++ js/util.js
@@ -55,7 +55,7 @@ const createCardEl = () => {
 
 			const cardImageEl = createElement('img', null, null, [
 				createElementAttribute('src', './images/drawers.jpg'),
-				createElementAttribute('alt', ''),
+				createElementAttribute('alt', 'Drawers'),
 				createElementAttribute('width', 280),
 				createElementAttribute('height', 278),
 			]);
